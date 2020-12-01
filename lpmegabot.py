36050a3b181624a40dc2e1965b364d09b305943b
mign0n <56875363+mign0n@users.noreply.github.com>
2020-12-01 21:19:41 +0500

    Implemented /next_full_moon command in the telegram chatbot.

--- lpmegabot.py
+++ lpmegabot.py
@@ -1,7 +1,7 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from datetime import date
+from datetime import datetime as dt
 from string import punctuation
 import ephem
 import settings
@@ -11,7 +11,7 @@ logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
                     filename="bot.log")
 
 API_KEY = settings.API_KEY
-DATE_TODAY = date.today().strftime("%Y/%m/%d")
+DATE_TODAY = dt.today().strftime("%Y/%m/%d")
 
 
 def greet_user(update, context):
@@ -20,13 +20,13 @@ def greet_user(update, context):
 
 
 def talk_to_me(update, context):
-    user_text = update.message.text
-    update.message.reply_text(user_text)
+    text = update.message.text
+    update.message.reply_text(text)
 
 
 def get_constelation(update, context):
-    user_text = update.message.text
-    _, planet = user_text.split()
+    text = update.message.text
+    _, planet = text.split()
     planet = planet.lower().capitalize()
     try:
         p = getattr(ephem, planet)(DATE_TODAY)
@@ -53,12 +53,27 @@ def wordcount(update, context):
     update.message.reply_text(result)
 
 
+def get_next_full_moon(update, context):
+    text = update.message.text
+    _, user_date = text.split()
+    try:
+        norm_date = dt.strptime(user_date, '%Y-%m-%d')
+    except ValueError:
+        norm_date = DATE_TODAY
+        update.message.reply_text("Enter the date in the format 'YYYY-MM-DD'.\n"
+                                  "Next full moon relative to today:")
+
+    result = ephem.next_full_moon(norm_date)
+    update.message.reply_text(result)
+
+
 def main():
     bot = Updater(API_KEY)
 
     dp = bot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", get_constelation))
+    dp.add_handler(CommandHandler("next_full_moon", get_next_full_moon))
     dp.add_handler((CommandHandler("wordcount", wordcount)))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     bot.start_polling()
