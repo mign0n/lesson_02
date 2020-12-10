30b9cc58bd598b354c10697bcb3b8523c978a693
mign0n <56875363+mign0n@users.noreply.github.com>
2020-12-10 07:34:31 +0000

    Level 3 (#1)
    * Updated ReadMe file.
    
    * Fixed some minor inaccuracies.
    
    * Implemented /cities command in the telegram chatbot.
    
    * Implemented /cities command in the telegram chatbot.
    
    * Optimized search for the desired city by starting letter:
    - Redesigned the method for storing city sets. Now cities are stored in a dictionary where keys are letters and values are sets of cities starting with this letter;
    - Redesigned `cities_game_logic` for working with new data structure.
    
    * The following improvements were made:
    - fixed a bug: the bot didn't recognize city names from more than 1 word;
    - added the bot's response to the `/cities` command without an argument;
    - fixed output city names according to the rules of the Russian language;
    - fixed some minor inaccuracies.
    
    * Implemented  command in chatbot
    
    * Updated requirements.
    
    * The following improvements were made:
    - the logic of the city game is wrapped in a class;
    - implemented recursive discarding of letters with which city names cannot begin;
    - updated letters with which city names cannot begin;
    - changed interface of function 'get_letter'.
    
    * Updated requirements.
    
    * Redisined calculator implementation.
    
    * Optimized calculator code.

--- lpmegabot.py
+++ lpmegabot.py
@@ -1,17 +1,21 @@
+import ephem
 import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from datetime import datetime as dt
-from string import punctuation
-import ephem
+import cities
 import settings
 
-logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
-                    level=logging.INFO,
-                    filename="bot.log")
+from datetime import datetime as dt
+from random import choice
+from string import punctuation
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 API_KEY = settings.API_KEY
 DATE_TODAY = dt.today().strftime("%Y/%m/%d")
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=logging.INFO,
+                    filename="bot.log")
+
+CITIES = cities.ru_cities
 
 
 def greet_user(update, context):
@@ -25,8 +29,7 @@ def talk_to_me(update, context):
 
 
 def get_constelation(update, context):
-    text = update.message.text
-    _, planet = text.split()
+    *_, planet = context.args
     planet = planet.lower().capitalize()
     try:
         p = getattr(ephem, planet)(DATE_TODAY)
@@ -35,15 +38,14 @@ def get_constelation(update, context):
         planet = "Moon"
         p = getattr(ephem, planet)(DATE_TODAY)
     try:
-        _, constellation = ephem.constellation(p)
+        *_, constellation = ephem.constellation(p)
         update.message.reply_text(f"Today {planet.capitalize()} is in the constellation of {constellation}.")
     except TypeError:
         update.message.reply_text("It's not a planet. Enter the name of a planet in the Solar System.")
 
 
 def wordcount(update, context):
-    _, *words = update.message.text.split()
-    text = ' '.join(words)
+    text = ' '.join(context.args)
     for char in punctuation:
         text = text.replace(char, '')
     n_words = len(text.split())
@@ -67,6 +69,108 @@ def get_next_full_moon(update, context):
     update.message.reply_text(result)
 
 
+def prettify_name(name):
+    if not name:
+        return
+    if ' ' in name:
+        p_name = ' '.join([x.capitalize() for x in name.split()])
+    elif '-' in name:
+        p_name = '-'.join([x.capitalize() for x in name.split('-') if len(x) > 2])
+    else:
+        p_name = name.capitalize()
+    return p_name
+
+
+class CitiesGame:
+    def __init__(self, remaining_cities):
+        self.bot_city = None
+        self.notice = None
+        self.impossible_start_letters = 'йьыъ'
+        self.last = -1
+        self.first = 0
+        self.remaining_cities = remaining_cities
+
+    def _get_letter(self, word, pos):
+        if word is None:
+            return
+        word = word.lower()
+        if word[pos] not in self.impossible_start_letters:
+            letter = word[pos]
+        else:
+            letter = self._get_letter(word[:self.last], self.last)
+        return letter
+
+    def logic(self, user_city, bot_city_prev):
+        last_letter_prev = self._get_letter(bot_city_prev, self.last)
+        first_letter = self._get_letter(user_city, pos=self.first)
+        if bot_city_prev is None or (first_letter == last_letter_prev):
+            user_city = user_city.lower()
+            if user_city in self.remaining_cities.get(first_letter, (None,)):
+                self.remaining_cities[first_letter].remove(user_city)
+                last_letter = self._get_letter(user_city, self.last)
+                self.bot_city = choice(list(self.remaining_cities.get(last_letter)))
+                self.remaining_cities[last_letter].remove(self.bot_city)
+            else:
+                user_city = prettify_name(user_city)
+                self.notice = f"Город с названием '{user_city}' уже называли, либо его не существует в России."
+        else:
+            self.notice = f"Введите название российского города на букву '{last_letter_prev}'."
+        return self.bot_city, self.remaining_cities, self.notice
+
+
+def cities_game(update, context):
+    game = CitiesGame(CITIES)
+    if not context.args:
+        response = "Ваш ход."
+    else:
+        user_city = ' '.join(context.args)
+        bot_city, remaining_cities, notice = game.logic(user_city, context.user_data.get('bot_city_prev'))
+        context.user_data['bot_city_prev'] = bot_city
+        context.user_data['remaining_cities'] = remaining_cities
+        bot_city = prettify_name(bot_city)
+        response = notice if notice is not None else f"{bot_city}, Ваш ход."
+    update.message.reply_text(response)
+
+
+def first_priority_ops(parts):
+    if isinstance(parts, list):
+        for i in range(len(parts)):
+            parts[i] = first_priority_ops(parts[i])
+        return parts[0] - sum(parts[1:])
+    elif isinstance(parts, str):
+        if '*' in parts:
+            result, *multipliers = map(first_priority_ops, parts.split('*'))
+            for multiplier in multipliers:
+                result *= multiplier
+            return result
+        elif '/' in parts:
+            result, *divisors = map(first_priority_ops, parts.split('/'))
+            for divisor in divisors:
+                result /= divisor
+            return result
+        else:
+            return float(parts)
+    return parts
+
+
+def last_priority_ops(expr):
+    parts = expr.replace(' ', '').split('+')
+    return sum([first_priority_ops(part.split('-')) for part in parts])
+
+
+def calc(update, context):
+    if not context.args:
+        return
+    expr = ''.join(context.args)
+    try:
+        result = last_priority_ops(expr)
+    except ZeroDivisionError:
+        result = "You can't divide by zero."
+    except ValueError:
+        result = "Invalid operands you entered. Try again."
+    update.message.reply_text(result)
+
+
 def main():
     bot = Updater(API_KEY)
 
@@ -75,7 +179,11 @@ def main():
     dp.add_handler(CommandHandler("planet", get_constelation))
     dp.add_handler(CommandHandler("next_full_moon", get_next_full_moon))
     dp.add_handler((CommandHandler("wordcount", wordcount)))
+    dp.add_handler((CommandHandler("cities", cities_game)))
+    dp.add_handler(CommandHandler("calc", calc))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
     bot.start_polling()
     bot.idle()
 
